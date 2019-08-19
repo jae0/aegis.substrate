44511964fd4d45b0f96b12b1b09f80806d42f4bd
Jae S Choi <choi.jae.seok@gmail.com>
2019-08-19 14:45:15 -0300

    misc updates

--- R/substrate.figures.R
+++ R/substrate.figures.R
@@ -6,7 +6,7 @@ substrate.figures = function( p=NULL, varnames="substrate.grainsize", datarange=
   #  if ( !exists("datadir", p) )   p$datadir  = file.path( p$data_root, "data" )
   #  if ( !exists("modeldir", p) )  p$modeldir = file.path( p$data_root, "modelled" )
 
-  z = bathymetry.db(p=p, DS="baseline")
+  z = bathymetry.db(spatial.domain=p$spatial.domain, DS="baseline")
   b = substrate.db( p=p, DS="complete" )
 #   lattice::levelplot( log(o$substrate.grainsize) ~ plon +plat, data=b, aspect="iso")
 
@@ -17,8 +17,8 @@ substrate.figures = function( p=NULL, varnames="substrate.grainsize", datarange=
     # if (length(oc) > 0) {
     #   b = b[oc,]
     # }
-
-    if (is.null( datarange)) datarange = quantile( b[,vn], probs=c(0.001, 0.999), na.rm=TRUE )
+    if (logyvar) b[,vn] = log( b[,vn] )
+    datarange = quantile( b[,vn], probs=c(0.001, 0.999), na.rm=TRUE )
   #  if (is.null( datarange)) datarange = range( b[,vn], na.rm=TRUE )
     dr = seq( datarange[1], datarange[2], length.out=100)
 
@@ -27,7 +27,7 @@ substrate.figures = function( p=NULL, varnames="substrate.grainsize", datarange=
       contour=FALSE, labels=FALSE, pretty=TRUE, xlab=NULL,ylab=NULL,scales=list(draw=FALSE),
       panel = function(x, y, subscripts, ...) {
         panel.levelplot (x, y, subscripts, aspect="iso", rez=c(1,1), ...)
-        sp.lines( isobath.db( p=p, DS="isobath", depths=isodepths, crs=p$internal.crs ), col = "gray80", cex=0.1 )
+        # sp.lines( isobath.db( p=p, DS="isobath", depths=isodepths, crs=p$internal.crs ), col = "gray80", cex=0.1 )
         sp.lines( coastline.db( p=p, crs=p$internal.crs ), col = "steelblue", cex=0.1 )
       }
     )
