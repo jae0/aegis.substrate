f3f24a888655e482c38bbeddb7393f5d01448e0d
Jae S Choi <choi.jae.seok@gmail.com>
2019-10-29 11:55:03 -0300

    lookup tidy

--- R/lookup_substrate_from_surveys.R
+++ R/lookup_substrate_from_surveys.R
@@ -1,10 +1,12 @@
 
 lookup_substrate_from_surveys = function( p, locs, vnames="substrate.grainsize.mean" ) {
 
-  B = substrate.db ( p=p, DS="aggregated_data" )  # 16 GB in RAM just to store!
-  varnames = unique( c( "plon", "plat", vnames ) )
-  domain_map = stmv::array_map( "xy->1", B[,c("plon","plat")], gridparams=p$gridparams )
-  locs_map = stmv::array_map( "xy->1", locs, gridparams=p$gridparams )
+  B = substrate.db ( p=p, DS="aggregated_data" )
+  locs = lonlat2planar( locs, proj.type=p$aegis_proj4string_planar_km )
+  locs$plon = round(locs$plon / p$inputdata_spatial_discretization_planar_km + 1 ) * p$inputdata_spatial_discretization_planar_km
+  locs$plat = round(locs$plat / p$inputdata_spatial_discretization_planar_km + 1 ) * p$inputdata_spatial_discretization_planar_km
+  locs_map = paste(locs$plon, locs$plat, sep=".")
+  domain_map = paste(B$plon, B$plat, sep=".")
   locs_index = match( locs_map, domain_map )
   out = B[locs_index, vnames]
   return(out)
