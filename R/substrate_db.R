3ec14b8392064020169a08a09e96787e442ab83a
Jae S Choi <choi.jae.seok@gmail.com>
2020-11-04 10:55:30 -0400

    sp to sf

--- R/substrate_db.R
+++ R/substrate_db.R
@@ -405,8 +405,8 @@
           contour=FALSE, labels=FALSE, pretty=TRUE, xlab=NULL,ylab=NULL,scales=list(draw=FALSE),
           panel = function(x, y, subscripts, ...) {
             panel.levelplot (x, y, subscripts, aspect="iso", rez=c(1,1), ...)
-            sp.lines( isobath_db( p=p, DS="isobath", depths=isodepths, project_to=p$aegis_proj4string_planar_km ), col = "gray80", cex=0.1 )
-            sp.lines( aegis.coastline::coastline_db( p=p, project_to=p$aegis_proj4string_planar_km, DS="gshhg coastline highres" ), col = "steelblue", cex=0.1 )
+            sp.lines( as( isobath_db( p=p, DS="isobath", depths=isodepths, project_to=p$aegis_proj4string_planar_km ), "Spatial"), col = "gray80", cex=0.1 )
+            sp.lines( as( aegis.coastline::coastline_db( p=p, project_to=p$aegis_proj4string_planar_km), col = "steelblue", cex=0.1 ), "Spatial" )
           }
         )
       )
