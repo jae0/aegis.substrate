3b8f60a9e17a3733777f7a4be09592e3ce2180dd
Jae S Choi <choi.jae.seok@gmail.com>
2020-12-10 21:15:55 -0400

    tweaks

--- R/substrate_parameters.R
+++ R/substrate_parameters.R
@@ -25,14 +25,14 @@ substrate_parameters = function( p=list(), project_name="substrate", project_cla
 
   p = parameters_add_without_overwriting( p,
     variabletomodel = "substrate.grainsize",
-    spatial_domain = "SSE",
+    spatial_domain = "canada.east.highres",
     spatial_domain_subareas = c( "canada.east",  "SSE", "SSE.mpa" , "snowcrab"),  # this is for bathymetry_db, not stmv
     aegis_dimensionality="space"
   )
 
   p = spatial_parameters( p=p)  # default (= only supported resolution of 0.2 km discretization)  .. do NOT change
 
-  p = parameters_add_without_overwriting( p, inputdata_spatial_discretization_planar_km = 0.5 )
+  p = parameters_add_without_overwriting( p, inputdata_spatial_discretization_planar_km = p$pres/2 )
    #  controls resolution of data prior to modelling (km .. ie 20 linear units smaller than the final discretization pres)
 
 
@@ -55,12 +55,12 @@ substrate_parameters = function( p=list(), project_name="substrate", project_cla
     p = parameters_add_without_overwriting( p,
       areal_units_source = "lattice", # "stmv_fields" to use ageis fields instead of carstm fields ... note variables are not the same
       areal_units_resolution_km = 5, # default in case not provided ... 25 km dim of lattice ~ 1 hr; 5km = 79hrs; 2km = ?? hrs
-      areal_units_proj4string_planar_km = aegis::projection_proj4string("utm20"),  # coord system to use for areal estimation and gridding for carstm
+      areal_units_proj4string_planar_km = p$aegis_proj4string_planar_km,  # coord system to use for areal estimation and gridding for carstm
       # areal_units_proj4string_planar_km = projection_proj4string("omerc_nova_scotia")  # coord system to use for areal estimation and gridding for carstm
       areal_units_overlay = "none",
       carstm_modelengine = "inla",  # {model engine}.{label to use to store}
       carstm_model_label = "default",
-      carstm_inputs_aggregated = FALSE
+      carstm_inputs_aggregated = TRUE
     )
 
     if ( !exists("carstm_model_call", p)  ) {
@@ -72,17 +72,22 @@ substrate_parameters = function( p=list(), project_name="substrate", project_cla
               + f(auid, model="bym2", graph=slot(sppoly, "nb"), scale.model=TRUE, constr=TRUE, hyper=H$bym2),
             family = "lognormal",
             data= M,
-            control.compute=list(dic=TRUE, waic=TRUE, cpo=TRUE, config=TRUE),
+            control.compute=list(dic=TRUE, waic=TRUE, cpo=TRUE, config=FALSE),
             control.results=list(return.marginals.random=TRUE, return.marginals.predictor=TRUE ),
             control.predictor=list(compute=FALSE, link=1 ),
             control.fixed=H$fixed,  # priors for fixed effects, generic is ok
-            control.inla = list(h=1e-4, tolerance=1e-9, cmin=0), # restart=3), # restart a few times in case posteriors are poorly defined
+            control.inla = list(h=1e-3, tolerance=1e-9, cmin=0), # restart=3), # restart a few times in case posteriors are poorly defined
             verbose=TRUE
           ) ' )
       }
     }
 
     p = carstm_parameters( p=p )  # fill in anything missing with defaults and do some checks
+
+    if ( p$inputdata_spatial_discretization_planar_km >= p$areal_units_resolution_km ) {
+      warning( "p$inputdata_spatial_discretization_planar_km >= p$areal_units_resolution_km " )
+    }
+    message ("p$areal_units_resolution_km: ", p$areal_units_resolution_km)
 
     return(p)
   }
@@ -91,55 +96,59 @@ substrate_parameters = function( p=list(), project_name="substrate", project_cla
   # ---------------------
 
   if (project_class %in% c("stmv" ,"default") ) {
+
+    p$libs = c( p$libs, project.library ( "stmv" ) )
     p$project_class="stmv"
 
     p = parameters_add_without_overwriting( p,
       DATA = 'substrate_db( p=p, DS="stmv_inputs" )',  # _highres
-      stmv_variables = list(Y="substrate.grainsize", LOCS=c("plon", "plat")),  # required as fft has no formulae
       stmv_model_label="default",
+      stmv_variables = list(Y="substrate.grainsize", LOCS=c("plon", "plat")),  # required as fft has no formulae
       stmv_global_modelengine = "gam",  # only marginally useful .. consider removing it and use "none",
-      stmv_global_modelformula = formula( paste(
-        p$variabletomodel, ' ~  s( log(z), k=3, bs="ts") + s( log(dZ), k=3, bs="ts" ) + s( log(ddZ), k=3, bs="ts" ) ') ),
-      stmv_global_family = gaussian(link="log"),
-      stmv_gam_optimizer = c("outer", "bfgs"),
       stmv_local_modelengine="fft",
-      stmv_fft_filter = "matern tapered lowpass modelled fast_predictions", #  act as a low pass filter first before matern with taper .. depth has enough data for this. Otherwise, use:
-      stmv_lowpass_nu = 0.5, # exp
-      stmv_lowpass_phi = stmv::matern_distance2phi( distance=0.1, nu=0.5, cor=0.1 ),
-      stmv_autocorrelation_fft_taper = 0.9,  # benchmark from which to taper
-      stmv_autocorrelation_localrange = 0.1,  # for output to stats
-      stmv_autocorrelation_interpolation = c(0.25, 0.1, 0.05, 0.01),
       stmv_variogram_method = "fft",
-      stmv_filter_depth_m = TRUE,  # need data above sea level to get coastline
+      stmv_filter_depth_m = 0.5,  # > 0.5 m 
       stmv_rsquared_threshold = 0.01, # lower threshold  .. ignore
       stmv_distance_statsgrid = 5, # resolution (km) of data aggregation (i.e. generation of the ** statistics ** )
-      stmv_distance_prediction_limits =c( 3, 25 ), # range of permissible predictions km (i.e 1/2 stats grid to upper limit based upon data density)
-      stmv_distance_scale = c( 5, 10, 20, 25, 40, 80, 150, 200), # km ... approx guesses of 95% AC range
-      stmv_distance_interpolation = c(  2.5 , 5, 10, 15, 20, 40, 80, 150, 200 ) , # range of permissible predictions km (i.e 1/2 stats grid to upper limit) .. in this case 5, 10, 20
       stmv_nmin = 90, # min number of data points req before attempting to model in a localized space
       stmv_nmax = 1000, # no real upper bound.. just speed /RAM
       stmv_force_complete_method = "linear_interp"
     )
+    
+    p = parameters_add_without_overwriting( p,
+      stmv_distance_prediction_limits = p$stmv_distance_statsgrid * c( 1/2, 5 ), # range of permissible predictions km (i.e 1/2 stats grid to upper limit based upon data density)
+      stmv_distance_scale = p$stmv_distance_statsgrid * c( 1, 2, 3, 4, 5, 10, 20, 40), # km ... approx guesses of 95% AC range
+      stmv_distance_interpolation = p$stmv_distance_statsgrid * c( 1/2, 1, 2, 3, 4, 5, 10, 20, 40),  # range of permissible predictions km (i.e 1/2 stats grid to upper limit) .. in this case 5, 10, 20
+      stmv_distance_interpolate_predictions = p$stmv_distance_statsgrid * c( 1/2, 1, 2, 3, 4, 8) # finalizing preds using linear interpolation
+    )
 
-    p$libs = c( p$libs, project.library ( "stmv" ) )
-
-
-    if (p$stmv_global_modelengine == "gam") {
-      p$libs = unique( c( p$libs, RLibrary ("mgcv")) )
-      if (!exists("stmv_gam_optimizer", p)) p$stmv_gam_optimizer = c("outer", "bfgs")
-      if (!exists("stmv_local_modelformula", p)) p$stmv_local_modelformula = formula( paste(
-        p$variabletomodel, ' ~ s(plon,k=3, bs="ts") + s(plat, k=3, bs="ts") + s(plon, plat, k=200, bs="ts")' ) )
-    }
 
     # some tweaked options for substrate
     if ( p$stmv_local_modelengine %in% c("krige" )) {
       # nothing to do ... faster than gstat
     }
 
+    if (p$stmv_global_modelengine == "gam") {
+      p$libs = unique( c( p$libs, RLibrary ("mgcv")))
+      p = parameters_add_without_overwriting( p,
+        stmv_global_modelformula = formula( paste(
+          p$variabletomodel, ' ~  1
+            + s( log(z), k=3, bs="ts") + s( log(dZ), k=3, bs="ts" ) + s( log(ddZ), k=3, bs="ts" ) 
+            + s( b.sdSpatial, k=3, bs="ts") + s( b.localrange, k=3, bs="ts") 
+          ') ),
+        stmv_global_family = gaussian(link="log"),
+        stmv_gam_optimizer = c("outer", "bfgs")
+      )
+    }
+
     if (p$stmv_local_modelengine == "gam") {
       p$libs = unique( c( p$libs, RLibrary ("mgcv")))
-      if (!exists("stmv_local_model_distanceweighted", p)) p$stmv_local_model_distanceweighted = TRUE
-      if (!exists("stmv_gam_optimizer", p)) p$stmv_gam_optimizer = c("outer", "bfgs")
+      p = parameters_add_without_overwriting( p,
+        stmv_local_modelformula = formula( paste(
+          p$variabletomodel, ' ~ s(plon,k=3, bs="ts") + s(plat, k=3, bs="ts") + s(plon, plat, k=200, bs="ts")') ),
+        stmv_local_model_distanceweighted = TRUE,
+        stmv_gam_optimizer = c("outer", "bfgs")
+      )
     }
 
     if ( p$stmv_local_modelengine == "bayesx" ) {
@@ -152,13 +161,43 @@ substrate_parameters = function( p=list(), project_name="substrate", project_cla
     }
 
     if ( p$stmv_local_modelengine == "fft" ) {
-      # definitely a cleaner (not overly smoothed) image than a GAM
-      # NOTE that  p$stmv_lowpass_phi and  p$stmv_lowpass_nu are very critical choices
-      # if (!exists("stmv_fft_filter", p)) p$stmv_fft_filter = "lowpass" # only act as a low pass filter .. depth has enough data for this. Otherwise, use:
-      # if (!exists("stmv_fft_filter", p)) p$stmv_fft_filter = "matern" to ~ krige
-      if (!exists("stmv_lowpass_phi", p)) p$stmv_lowpass_phi = p$pres*2 # FFT based method when operating gloablly
-      if (!exists("stmv_lowpass_nu", p)) p$stmv_lowpass_nu = 0.5 # this is exponential covar
+      nu = 0.5  # exponential smoothing
+      ac_local = 0.1  # ac at which to designate "effective range"
+      p = parameters_add_without_overwriting( p,
+        stmv_fft_filter = "matern tapered lowpass modelled fast_predictions", #  act as a low pass filter first before matern with taper  
+        # stmv_fft_filter = "matern_tapered_modelled",
+        stmv_autocorrelation_fft_taper = 0.75,  # benchmark from which to taper
+        stmv_autocorrelation_localrange = ac_local,  # for output to stats
+        stmv_autocorrelation_interpolation = c(0.25, 0.1, 0.05, 0.01),
+        stmv_lowpass_nu = nu, # exp
+        stmv_lowpass_phi = stmv::matern_distance2phi( distance=p$pres/2, nu=nu, cor=ac_local )
+      )
     }
+
+    # default to serial mode
+    p = parameters_add_without_overwriting( p,
+      stmv_runmode = list(
+        globalmodel = TRUE,
+        scale = rep("localhost", 1),
+        interpolate = list(
+          cor_0.25 = rep("localhost", 1),
+          cor_0.1  = rep("localhost", 1),
+          cor_0.05 = rep("localhost", 1),
+          cor_0.01 = rep("localhost", 1)
+        ),
+        interpolate_predictions = list(
+          c1 = rep("localhost", 1),  
+          c2 = rep("localhost", 1),  
+          c3 = rep("localhost", 1),
+          c4 = rep("localhost", 1),
+          c5 = rep("localhost", 1),
+          c6 = rep("localhost", 1),
+          c7 = rep("localhost", 1)
+        ),
+        save_intermediate_results = TRUE,
+        save_completed_data = TRUE # just a dummy variable with the correct name
+      )
+    )
 
     p = aegis_parameters( p=p, DS="stmv" )  # get defaults
 
@@ -176,6 +215,7 @@ substrate_parameters = function( p=list(), project_name="substrate", project_cla
   if (project_class %in% c("hybrid") ) {
 
     p$project_class="hybrid"
+    p$libs = unique( c( p$libs, RLibrary ("carstm", "stmv", "INLA" )) )
 
     p = parameters_add_without_overwriting( p,
       DATA = 'substrate_db( p=p, DS="stmv_inputs" )',  # _highres
@@ -183,11 +223,13 @@ substrate_parameters = function( p=list(), project_name="substrate", project_cla
       stmv_model_label="default",
       stmv_global_modelengine = "gam",  # only marginally useful .. consider removing it and use "none",
       stmv_global_modelformula = formula( paste(
-        p$variabletomodel, ' ~  s( log(z), k=3, bs="ts") + s( log(dZ), k=3, bs="ts" ) + s( log(ddZ), k=3, bs="ts" ) ') ),
+        p$variabletomodel, ' ~  1 
+          + s( log(z), k=3, bs="ts") + s( log(dZ), k=3, bs="ts" ) + s( log(ddZ), k=3, bs="ts" ) 
+          + s( b.sdSpatial, k=3, bs="ts") + s( b.localrange, k=3, bs="ts") 
+        ') ),
       stmv_global_family = gaussian(link="log"),
-      stmv_gam_optimizer = c("outer", "bfgs"),
       stmv_local_modelengine="carstm",
-      stmv_local_covariates_carstm = "",  # only model covariates
+      stmv_local_covariates_carstm = "",  # only model covariates globally
       stmv_local_all_carstm = "",  # ignoring au
       stmv_local_modelcall = paste(
         'inla(
@@ -203,20 +245,28 @@ substrate_parameters = function( p=list(), project_name="substrate", project_cla
         ) '
       ),   # NOTE:: this is a local model call
       stmv_distance_statsgrid = 1, # resolution (km) of data aggregation (i.e. generation of the ** statistics ** )
-    #  stmv_interpolation_distance = 5,   # fixed distance 2 x statsgrid
-      stmv_distance_prediction_limits =c( 5, 25 ), # range of permissible predictions km (i.e 1/2 stats grid to upper limit based upon data density)
-      stmv_distance_interpolation = c(5, 10),
-      stmv_nmin = 10, # min number of data points req before attempting to model in a localized space
-      stmv_nmax = 1000, # no real upper bound.. just speed /RAM
+      stmv_nmin = 100, # min number of data points req before attempting to model in a localized space
+      stmv_nmax = 1000 # no real upper bound.. just speed /RAM
+    )
+
+ 
+    p = parameters_add_without_overwriting( p,
+      stmv_distance_prediction_limits = p$stmv_distance_statsgrid * c( 1, 5 ), # range of permissible predictions km (i.e  stats grid to upper limit based upon data density)
+      stmv_distance_interpolation = p$stmv_distance_statsgrid * c( 1/2, 1, 2, 3, 4, 5),  # range of permissible predictions km (i.e 1/2 stats grid to upper limit) .. in this case 5, 10, 20
+      stmv_distance_interpolate_predictions = p$stmv_distance_statsgrid * c( 1/2, 1, 2, 3, 4, 5) # finalizing preds using linear interpolation
+    )
+
+    p = parameters_add_without_overwriting( p,
       stmv_runmode = list(
         carstm = rep("localhost", 1),
         globalmodel = FALSE,
-        # restart_load = "interpolate_correlation_basis_0.01" ,  # only needed if this is restarting from some saved instance
         save_intermediate_results = TRUE,
         save_completed_data = TRUE
-      )  # ncpus for each runmode
+      )  
     )
+
     p = aegis_parameters( p=p, DS="stmv" )  # get defaults
+
 
     if ( p$inputdata_spatial_discretization_planar_km >= p$pres ) {
       warning( "p$inputdata_spatial_discretization_planar_km >= p$pres " )
--- inst/scripts/03_substrate_stmv.R
+++ inst/scripts/03_substrate_stmv.R
@@ -2,75 +2,54 @@
 ## NOTE:: substrate size is really only relevant for SSE/snowcrab domain right now as no
 ##        other data source has been found/identified
 ##        but working at the size of canada.east.highres for compatibility with bathymetry
+
 ## TODO:: add data collected by snow crab survey and any others for that matter
 
+## TODO --- once settled, these params should be moved into substrate_parameters as defaults
 
-# about 1.5 hr
-scale_ram_required_main_process = 2 # GB twostep / fft
-scale_ram_required_per_process  = 1 # twostep / fft /fields vario ..  (mostly 0.5 GB, but up to 5 GB)
-scale_ncpus = min( parallel::detectCores(), floor( (ram_local()- scale_ram_required_main_process) / scale_ram_required_per_process ) )
+require(aegis.substrate)
 
-# nn hrs
-interpolate_ram_required_main_process = 2 # GB twostep / fft
-interpolate_ram_required_per_process  = 2  # twostep / fft /fields vario ..
-interpolate_ncpus = min( parallel::detectCores(), floor( (ram_local()- interpolate_ram_required_main_process) / interpolate_ram_required_per_process ) )
-# interpolate_ncpus = 8
-
-p = aegis.substrate::substrate_parameters(
+p = substrate_parameters(
   project_class="stmv",
-  data_root = project.datadirectory( "aegis", "substrate" ),
-  DATA = 'substrate_db( p=p, DS="stmv_inputs" )',
-  spatial_domain = "canada.east.highres" ,
-  spatial_domain_subareas = c( "canada.east", "SSE", "snowcrab", "SSE.mpa" ),
-  inputdata_spatial_discretization_planar_km = 0.5 / 2, # 0.5==p$pres; controls resolution of data prior to modelling (km .. ie 20 linear units smaller than the final discretization pres)
-  aegis_dimensionality="space",
-  stmv_variables = list(Y="substrate.grainsize"),
-  stmv_global_modelengine = "gam",
-  stmv_global_modelformula = formula( paste(
-    'substrate.grainsize ',
-    ' ~ s( b.sdSpatial, k=3, bs="ts") + s( b.localrange, k=3, bs="ts") ',
-    ' + s(log(z), k=3, bs="ts") + s(log(dZ), k=3, bs="ts") +s(log(ddZ), k=3, bs="ts") '
-  ) ),
-  stmv_global_family = gaussian(link="log"),
-  stmv_local_modelengine="fft",  # currently the perferred approach
-  stmv_fft_filter = "matern_tapered_modelled", #
-  # stmv_lowpass_nu = 0.1,
-  # stmv_lowpass_phi = stmv::matern_distance2phi( distance=0.25, nu=0.1, cor=0.5 ), # default p$res = 0.5;
-  stmv_autocorrelation_fft_taper = 0.75,  # benchmark from which to taper
-  stmv_autocorrelation_localrange = 0.1,  # for output to stats
-  stmv_autocorrelation_interpolation = c(0.25, 0.1, 0.05, 0.01 ),
-  stmv_variogram_method = "fft",
-  stmv_filter_depth_m = 0.1, # the depth covariate is input in m, so, choose stats locations with elevation > 0 m as being on land
-  stmv_local_model_distanceweighted = TRUE,
-  stmv_rsquared_threshold = 0.1, # lower threshold == ignore
-  stmv_distance_statsgrid = 5, # resolution (km) of data aggregation (i.e. generation of the ** statistics ** )
-  stmv_distance_prediction_limits =c( 4, 40 ), # range of permissible predictions km (i.e 1/2 stats grid to upper limit based upon data density)
-  stmv_distance_scale = c( 5, 10, 25, 50, 75, 150 ), # km ... approx guess of 95% AC range
+  stmv_distance_prediction_limits = p$stmv_distance_statsgrid * c( 1/2, 5 ), # range of permissible predictions km (i.e 1/2 stats grid to upper limit based upon data density)
+  stmv_distance_scale = p$stmv_distance_statsgrid * c( 1, 2, 3, 4, 5, 10, 20, 40), # km ... approx guesses of 95% AC range
+  stmv_distance_interpolation = p$stmv_distance_statsgrid * c( 1/2, 1, 2, 3, 4, 5, 10, 20, 40),  # range of permissible predictions km (i.e 1/2 stats grid to upper limit) .. in this case 5, 10, 20
+  stmv_distance_interpolate_predictions = p$stmv_distance_statsgrid * c( 1/2, 1, 2, 3, 4, 8), # finalizing preds using linear interpolation
   stmv_nmin = 100, # stmv_nmin/stmv_nmax changes with resolution
-  stmv_nmax = 400, # numerical time/memory constraint -- anything larger takes too much time .. anything less .. errors
-  stmv_runmode = list(
-    globalmodel = FALSE,
-    scale = rep("localhost", scale_ncpus),
-    interpolate = list(
-      cor_0.25 = rep("localhost", interpolate_ncpus),
-      cor_0.1 = rep("localhost", interpolate_ncpus-2),
-      cor_0.05 = rep("localhost", max(1, interpolate_ncpus-3)),
-      cor_0.01 = rep("localhost", max(1, interpolate_ncpus-3))
-    ),
-    interpolate_predictions = list(
-      c1 = rep("localhost", max(1, interpolate_ncpus-1)),  # ncpus for each runmode
-      c2 = rep("localhost", max(1, interpolate_ncpus-1)),  # ncpus for each runmode
-      c3 = rep("localhost", max(1, interpolate_ncpus-2)),
-      c4 = rep("localhost", max(1, interpolate_ncpus-3)),
-      c5 = rep("localhost", max(1, interpolate_ncpus-4)),
-      c6 = rep("localhost", max(1, interpolate_ncpus-4)),
-      c7 = rep("localhost", max(1, interpolate_ncpus-5))
-    ),
-    restart_load = "interpolate_correlation_basis_0.01" ,  # only needed if this is restarting from some saved instance
-    save_intermediate_results = TRUE,
-    save_completed_data = TRUE # just a dummy variable with the correct name
-  )
+  stmv_nmax = 400 # numerical time/memory constraint -- anything larger takes too much time .. anything less .. errors
 )
+
+
+if (0) {
+    # default is serial mode .. to enable multicore:
+    scale_ncpus = ram_local( "ncores", ram_main=2, ram_process=1 ) # in GB; about 24 hr
+    interpolate_ncpus = ram_local( "ncores", ram_main=2, ram_process=2 ) # nn hrs
+
+    p = parameters_add_without_overwriting( p,
+      stmv_runmode = list(
+        globalmodel = TRUE,
+        scale = rep("localhost", scale_ncpus),
+        interpolate = list(
+          cor_0.25 = rep("localhost", interpolate_ncpus),
+          cor_0.1  = rep("localhost", interpolate_ncpus),
+          cor_0.05 = rep("localhost", interpolate_ncpus),
+          cor_0.01 = rep("localhost", interpolate_ncpus)
+        ),
+        interpolate_predictions = list(
+          c1 = rep("localhost", interpolate_ncpus),  
+          c2 = rep("localhost", interpolate_ncpus),  
+          c3 = rep("localhost", interpolate_ncpus),
+          c4 = rep("localhost", interpolate_ncpus),
+          c5 = rep("localhost", interpolate_ncpus),
+          c6 = rep("localhost", interpolate_ncpus),
+          c7 = rep("localhost", interpolate_ncpus)
+        ),
+        save_intermediate_results = TRUE,
+        save_completed_data = TRUE # just a dummy variable with the correct name
+      )
+    )
+
+}
 
 
 stmv( p=p )
