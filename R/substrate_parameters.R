dec2b3e113a2ef5b7a82745008329b4e2f3e2f5a
Jae S Choi <choi.jae.seok@gmail.com>
2021-01-12 12:32:57 -0400

    tweaks to tessilation params

--- R/substrate_parameters.R
+++ R/substrate_parameters.R
@@ -58,6 +58,11 @@ substrate_parameters = function( p=list(), project_name="substrate", project_cla
       areal_units_proj4string_planar_km = p$aegis_proj4string_planar_km,  # coord system to use for areal estimation and gridding for carstm
       # areal_units_proj4string_planar_km = projection_proj4string("omerc_nova_scotia")  # coord system to use for areal estimation and gridding for carstm
       areal_units_overlay = "none",
+      areal_units_timeperiod = "none"
+      tus="none", 
+      fraction_cv = 1.0, 
+      fraction_good_bad = 0.9, 
+      nAU_min = 30,  
       carstm_modelengine = "inla",  # {model engine}.{label to use to store}
       carstm_model_label = "default",
       carstm_inputs_aggregated = TRUE
