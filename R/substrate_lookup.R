0162f695e0078c4622afa4e30330167c3ca8efe3
Jae S Choi <choi.jae.seok@gmail.com>
2021-02-03 09:24:31 -0400

    misc

--- R/substrate_lookup.R
+++ R/substrate_lookup.R
@@ -1,5 +1,5 @@
 substrate_lookup = function( LOCS=NULL, spatial_domain=NULL, lookup_from="core", lookup_to="points", FUNC=mean,  vnames="substrate.grainsize", lookup_from_class="aggregated_data"  ) {
-  # lookup from rawdata
+ 
  # substrate_lookup( LOCS=M[, c("lon", "lat")], spatial_domain=p$spatial_domain, lookup_from="core", lookup_to="points" , lookup_from_class="aggregated_data" ) # core=="rawdata"
 
   if (is.null(spatial_domain))  {
@@ -12,43 +12,55 @@ substrate_lookup = function( LOCS=NULL, spatial_domain=NULL, lookup_from="core",
 
 
   if ( lookup_from %in% c("core") & lookup_to == "points" )  {
-    # matching to point to point 
-    # if any still missing then use stmv depths
-    LU = substrate_db ( p=pB, DS=lookup_from_class )  # raw data
-    LU = planar2lonlat(LU, pB$aegis_proj4string_planar_km)
+    # matching to point (LU) to point (LOCS) 
+    vn = "substrate.grainsize"
+    vn2 = paste( vn, "mean", sep="." )
 
-    vn2 = "substrate.grainsize.mean"
-    
+    pB = bathymetry_parameters( spatial_domain=pS$spatial_domain, project_class=lookup_from  )
+    BA = bathymetry_db ( pB, DS="baseline", varnames=c("lon", "lat")  )
+    BA = planar2lonlat(BA, pB$aegis_proj4string_planar_km)
+
+    LU = substrate_db ( p=pB, DS=lookup_from_class )  # raw data
+    names(LU)[ which(names(LU) == vn2 ) ] =  vn
+
     LOCS = lonlat2planar(LOCS, proj.type=pB$aegis_proj4string_planar_km) # get planar projections of lon/lat in km
-    LOCS[, vnames] = LU[ match(
+    LOCS[, vn] = LU[ match(
         array_map( "xy->1", LOCS[, c("plon","plat")], gridparams=pB$gridparams ),
-        array_map( "xy->1", LU[,c("plon","plat")], gridparams=pB$gridparams )
-    ), vn2 ]
-    return( LOCS[,vnames] )
+        array_map( "xy->1", BA[,c("plon","plat")], gridparams=pB$gridparams )
+    ), vn ]
+    return( LOCS[,vn] )
   }
 
+
   if ( lookup_from %in% c("core") & lookup_to == "areal_units" )  {
-    # point -> areal unit
+    # point (LU) -> areal unit (LOCS)
+    vn = "substrate.grainsize"
+    vn2 = paste( vn, "mean", sep="." )
+
+    pB = bathymetry_parameters( spatial_domain=pS$spatial_domain, project_class=lookup_from  )
+    BA = bathymetry_db ( pB, DS="baseline", varnames=c("lon", "lat")  )
+    BA = planar2lonlat(BA, pB$aegis_proj4string_planar_km)
+
     LU = substrate_db ( p=pB, DS=lookup_from_class )  # raw data
-    LU = planar2lonlat(LU, pB$aegis_proj4string_planar_km)
-    
+    LU = cbind( LU, BA )
     LU = sf::st_as_sf( LU, coords=c("lon", "lat") )
     st_crs(LU) = st_crs( projection_proj4string("lonlat_wgs84") )
     LU = sf::st_transform( LU, crs=st_crs(LOCS) )
-    vn2 = "substrate.grainsize.mean"
-    LOCS[, vnames] = aggregate( LU[, vn2], LOCS, FUNC, na.rm=TRUE ) [[vn2]] [iAS]
-    return( LOCS[,vnames] )
+
+    LOCS[, vn] = aggregate( LU[, vn2], LOCS, FUNC, na.rm=TRUE ) [[vn2]] 
+    return( st_drop_geometry(LOCS)[,vn] )
   }
 
 
   if ( lookup_from %in% c("stmv", "hybrid") & lookup_to == "points" )  {
-    # matching to point to point 
+    # matching to point (LU) to point (LOCS)
     # if any still missing then use stmv depths
     pB = bathymetry_parameters( spatial_domain=pS$spatial_domain, project_class=lookup_from  )
     BA = bathymetry_db ( pB, DS="baseline", varnames=c("lon", "lat")  )
+    BA = planar2lonlat(BA, proj.type=pS$aegis_proj4string_planar_km)
+
     LU = substrate_db ( pS, DS="complete", varnames="all" )  # raw data
     LU = cbind(LU, BA )
-    LU = planar2lonlat(LU, proj.type=pS$aegis_proj4string_planar_km)
     
     LOCS = lonlat2planar(LOCS, proj.type=pB$aegis_proj4string_planar_km) # get planar projections of lon/lat in km
     LOCS[,vnames] = LU[ match(
@@ -59,62 +71,75 @@ substrate_lookup = function( LOCS=NULL, spatial_domain=NULL, lookup_from="core",
   }
 
   if ( lookup_from %in% c("stmv", "hybrid") & lookup_to == "areal_units" )  {
-    # point -> areal unit
+    # point (LU) -> areal unit (LOCS)
     pB = bathymetry_parameters( spatial_domain=pS$spatial_domain, project_class=lookup_from  )
     BA = bathymetry_db ( pB, DS="baseline", varnames=c("lon", "lat")  )
+    BA = planar2lonlat(BA, pB$aegis_proj4string_planar_km)
+    
     LU = substrate_db ( pS, DS="complete", varnames="all" )  # raw data
     LU = cbind(LU, BA )
-    LU = planar2lonlat(LU, pB$aegis_proj4string_planar_km)
-    
     LU = sf::st_as_sf( LU, coords=c("lon", "lat") )
     st_crs(LU) = st_crs( projection_proj4string("lonlat_wgs84") )
     LU = sf::st_transform( LU, crs=st_crs(LOCS) )
+
     for (vn in vnames) {
-      LOCS[, vn] = aggregate( LU[, vn], LOCS, FUNC, na.rm=TRUE ) [[vn]] [iAS]
+      LOCS[, vn] = aggregate( LU[, vn], LOCS, FUNC, na.rm=TRUE ) [[vn]] 
     }
-    return( LOCS[,vnames] )
+    return( st_drop_geometry(LOCS)[,vnames] )
   }
 
 
 
   if ( lookup_from %in% c("carstm" ) & lookup_to == "points" )  {
-    # point to areal unit
-    LU = carstm_model( p=pB, DS="carstm_modelled_summary" ) 
-    if (is.null(LU)) stop("Carstm predicted fields not found")
+    #  areal unit (LU) to point (LOCS)
+    LU_summ = carstm_model( p=pB, DS="carstm_modelled_summary" ) 
+    if (is.null(LU_summ)) stop("Carstm predicted fields not found")
 
-    AU = areal_units( p=pB )  #  poly associated with LU
-    bm = match( AU$AUID, LU$AUID )
-    AU[[vnames]] = LU[,vnames][ bm ]
-    LU = NULL
-    # now rasterize and re-estimate
+    LU = areal_units( p=pB )  #  poly associated with LU
+    LU = sf::st_transform( LU, crs=st_crs(pB$aegis_proj4string_planar_km) )
+    bm = match( LU$AUID, LU_summ$AUID )
+    LU[[vnames]] = LU_summ[,vnames][ bm ]
+    LU_summ = NULL
 
-    LOCS = lonlat2planar(LOCS, proj.type=pB$aegis_proj4string_planar_km) # get planar projections of lon/lat in km
-    raster_template = raster( LOCS, res=pB$areal_units_resolution_km, crs=st_crs( LOCS ) ) # +1 to increase the area
+    LOCS = sf::st_as_sf( LOCS, coords=c("lon", "lat") )
+    st_crs(LOCS) = st_crs( projection_proj4string("lonlat_wgs84") )
+    LOCS = sf::st_transform( LOCS, crs=st_crs(pB$aegis_proj4string_planar_km) )
+
+    raster_template = raster( LOCS, res=min(pB$gridparams$res), crs=st_crs( LOCS ) ) 
     for (vn in vnames) {
-      LL = fasterize::fasterize( AU, raster_template, field=vn )
-      LOCS[[vn]] = sp::over( LOCS, LL[, vn ], fn=FUNC, na.rm=TRUE )
+      LL = fasterize::fasterize( LU, raster_template, field=vn )
+      o = sf::st_as_sf( as.data.frame( raster::rasterToPoints(LL)), coords=c("x", "y") )
+      st_crs(o) = st_crs( LOCS )
+    
+      LOCS[,vn] = st_drop_geometry(o)[ match(
+        array_map( "xy->1", st_coordinates(LOCS), gridparams=pB$gridparams ),
+        array_map( "xy->1", st_coordinates(o), gridparams=pB$gridparams )
+      ), "layer" ]
     }
-    return( LOCS[,vnames] )
+    
+    return( st_drop_geometry(LOCS)[,vnames] )
   }
 
 
   if ( lookup_from %in% c("carstm") & lookup_to == "areal_units" )  {
-    # areal unit to areal unit
-    LU = carstm_model( p=pB, DS="carstm_modelled_summary" ) 
-    if (is.null(LU)) stop("Carstm predicted fields not found")
+    # areal unit (LU) to areal unit (LOCS)
+    LU_summ = carstm_model( p=pB, DS="carstm_modelled_summary" ) 
+    if (is.null(LU_summ)) stop("Carstm predicted fields not found")
 
-    AU = areal_units( p=pB )  #  poly associated with LU
-    bm = match( AU$AUID, LU$AUID )
-    AU[[vnames]] = LU[,vnames][ bm ]
-    LU = NULL
+    LU = areal_units( p=pB )  #  poly associated with LU
+    bm = match( LU$AUID, LU_summ$AUID )
+    LU[[vnames]] = LU_summ[,vnames][ bm ]
+    LU_summ = NULL
 
     # now rasterize and re-estimate
-    raster_template = raster( LOCS, res=pB$areal_units_resolution_km, crs=st_crs( LOCS ) ) # +1 to increase the area
+    raster_template = raster( LOCS, res=min(pB$gridparams$res), crs=st_crs( LOCS ) ) 
     for (vn in vnames) {
-      LL = fasterize::fasterize( AU, raster_template, field=vn )
-      LOCS[[vn]] = sp::over( LOCS, LL[, vn ], fn=FUNC, na.rm=TRUE )
+      LL = fasterize::fasterize( LU, raster_template, field=vn )
+      o = sf::st_as_sf( as.data.frame( raster::rasterToPoints(LL)), coords=c("x", "y") )
+      st_crs(o) = st_crs( LOCS )
+      LOCS[, vn] = sf:::aggregate.sf( o, LOCS, FUNC, na.rm=TRUE ) [["layer"]]  
     }
-    return( LOCS[,vnames] )
+    return( st_drop_geometry(LOCS)[,vnames] )
   } 
 
 }
