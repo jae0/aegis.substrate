56869f2a9c6b342f07f312ac6a528eb66d99177f
Jae S Choi <choi.jae.seok@gmail.com>
2022-01-26 16:41:52 -0400

    misc

--- inst/scripts/03_substrate_carstm.R
+++ inst/scripts/03_substrate_carstm.R
@@ -31,64 +31,65 @@
   
     }
 
-# run model and obtain predictions
-  fit = carstm_model( 
-    p=p, 
-    sppoly=sppoly,
-    data= substrate_db( p=p, DS="carstm_inputs", sppoly=sppoly), 
-    num.threads="4:2",
-    redo_fit = TRUE,  
-    verbose=TRUE 
-  ) 
-  # fit = carstm_model( p=p, DS="carstm_modelled_fit" )  # extract currently saved model fit
+  # run model and obtain predictions
+    fit = carstm_model( 
+      p=p, 
+      sppoly=areal_units( p=p  ),
+      data= substrate_db( p=p, DS="carstm_inputs", sppoly=sppoly), 
+      num.threads="4:2",
+      theta = c( 1.710, 3.729, 0.008, 5.591 ), 
+      redo_fit = TRUE,  
+      verbose=TRUE 
+    ) 
+    # fit = carstm_model( p=p, DS="carstm_modelled_fit" )  # extract currently saved model fit
+    
+      # extract results
+      if (0) {
+        fit = carstm_model( p=p, data=M, sppoly=sppoly ) # alt way of running
+        # very large files .. slow 
+        fit = carstm_model( p=p, DS="carstm_modelled_fit", sppoly=sppoly )  # extract currently saved model fit
+    
+        fit$summary$dic$dic
+        fit$summary$dic$p.eff
+        fit$dyear
+
+        plot(fit)
+        plot(fit, plot.prior=TRUE, plot.hyperparameters=TRUE, plot.fixed.effects=FALSE )
+      }
+
+  # extract results and examine
+    res = carstm_model( p=p, DS="carstm_modelled_summary", sppoly=sppoly  ) # to load currently saved results
+    res$summary
+
+    
+    # maps of some of the results
+    outputdir = file.path(p$data_root, "maps", p$carstm_model_label )
+    if ( !file.exists(outputdir)) dir.create( outputdir, recursive=TRUE, showWarnings=FALSE )
+
+    tmout = carstm_map(  res=res, vn="predictions", 
+        sppoly=sppoly,
+        palette="viridis",
+        title="Substrate grainsize (mm)", 
+        plot_elements=c( "isobaths",  "compass", "scale_bar", "legend" ),
+        tmap_zoom= c((p$lon0+p$lon1)/2 - 0.5, (p$lat0+p$lat1)/2 -0.8, 6.5)
+    )  
+    tmout
+
+    outfilename= file.path( outputdir, paste("substrate_grain_size_carstm", "png", sep=".") )
+    mapview::mapshot( tmap_leaflet(tmout), file=outfilename, vwidth = 1600, vheight = 1200 )
+    
+  # random effects  ..i.e.,  deviation from lognormal model
+    tmout = carstm_map(  res=res, vn= c( "random", "space", "combined" ), 
+        sppoly=sppoly,
+        palette="viridis",
+        title="Substrate grainsize spatial errors (mm)",
+        plot_elements=c( "isobaths",  "compass", "scale_bar", "legend" ),
+        tmap_zoom= c((p$lon0+p$lon1)/2-0.5, (p$lat0+p$lat1)/2 -0.8, 6.5)
+    )  
+    tmout
   
-    # extract results
-    if (0) {
-      fit = carstm_model( p=p, data=M, sppoly=sppoly ) # alt way of running
-      # very large files .. slow 
-      fit = carstm_model( p=p, DS="carstm_modelled_fit", sppoly=sppoly )  # extract currently saved model fit
-  
-      fit$summary$dic$dic
-      fit$summary$dic$p.eff
-      fit$dyear
+    outfilename= file.path( outputdir, paste("substrate_grain_size_spatialeffect_carstm", "png", sep=".") )
+    mapview::mapshot( tmap_leaflet(tmout), file=outfilename, vwidth = 1600, vheight = 1200 )
+    
 
-      plot(fit)
-      plot(fit, plot.prior=TRUE, plot.hyperparameters=TRUE, plot.fixed.effects=FALSE )
-    }
-
-# extract results and examine
-  res = carstm_model( p=p, DS="carstm_modelled_summary", sppoly=sppoly  ) # to load currently saved results
-  res$summary
-
-  
-  # maps of some of the results
-  outputdir = file.path(p$data_root, "maps", p$carstm_model_label )
-  if ( !file.exists(outputdir)) dir.create( outputdir, recursive=TRUE, showWarnings=FALSE )
-
-  tmout = carstm_map(  res=res, vn="predictions", 
-      sppoly=sppoly,
-      palette="viridis",
-      title="Substrate grainsize (mm)", 
-      plot_elements=c( "isobaths", "coastline", "compass", "scale_bar", "legend" ),
-      tmap_zoom= c((p$lon0+p$lon1)/2 - 0.5, (p$lat0+p$lat1)/2 -0.8, 6.5)
-  )  
-  tmout
-
-  outfilename= file.path( outputdir, paste("substrate_grain_size_carstm", "png", sep=".") )
-  mapview::mapshot( tmap_leaflet(tmout), file=outfilename, vwidth = 1600, vheight = 1200 )
-  
-# random effects  ..i.e.,  deviation from lognormal model
-  tmout = carstm_map(  res=res, vn= c( "random", "space", "combined" ), 
-      sppoly=sppoly,
-      palette="viridis",
-      title="Substrate grainsize spatial errors (mm)",
-      plot_elements=c( "isobaths", "coastline", "compass", "scale_bar", "legend" ),
-      tmap_zoom= c((p$lon0+p$lon1)/2-0.5, (p$lat0+p$lat1)/2 -0.8, 6.5)
-  )  
-  tmout
- 
-  outfilename= file.path( outputdir, paste("substrate_grain_size_spatialeffect_carstm", "png", sep=".") )
-  mapview::mapshot( tmap_leaflet(tmout), file=outfilename, vwidth = 1600, vheight = 1200 )
-  
-
-# end
+  # end
