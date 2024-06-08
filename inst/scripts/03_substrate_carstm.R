dc02b54e4ccbd085d3c8ee6f0c5e8438d692eb39
Jae S Choi <choijaeseok@gmail.com>
2024-06-07 23:02:36 -0300

    misc

--- inst/scripts/03_substrate_carstm.R
+++ inst/scripts/03_substrate_carstm.R
@@ -111,6 +111,18 @@
     #,
     #brks=seq(1, 501, 100) )
 
+  # random effects  ..i.e.,  deviation from lognormal model ( pure spatial effect )
+    outfilename= file.path( outputdir, paste("substrate_grain_size_spatialeffect_carstm", "png", sep=".") )
+    plt = carstm_map(  res=res, vn= c( "random", "space", "re" ), 
+        sppoly=sppoly,
+        title="Substrate grainsize spatial errors (mm)",
+        colors=rev(RColorBrewer::brewer.pal(5, "RdYlBu")),
+        additional_features=additional_features,
+        outfilename=outfilename
+    )  
+    plt
+  
+ 
 
 
 
