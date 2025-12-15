8efb1f4691ab748b18c6cf789e325f33a66c4841
jae0 <2574393+jae0@users.noreply.github.com>
2025-12-15 10:35:19 -0400

    mesh params

--- inst/scripts/03_substrate_carstm.R
+++ inst/scripts/03_substrate_carstm.R
@@ -5,6 +5,14 @@
   p = aegis.substrate::substrate_parameters( project_class="carstm", areal_units_resolution_km=5  )  #10 km grid gives about 2000 au .. in the right range for optimal solutions that are not too slow, 5 km gives stable results .. 
 
 
+  # bbox = c(-71.5, 41, -52.5,  50.5 )
+  additional_features = features_to_add( 
+      p=p, 
+      isobaths=c( 100, 200, 300, 400, 500  ), 
+      xlim=c(-80,-40), 
+      ylim=c(38, 60) 
+  )
+  
     # adjust based upon RAM requirements and ncores
     # inla.setOption( num.threads="6:2" )
     # inla.setOption(blas.num.threads= 3 )
@@ -20,7 +28,10 @@
       # xydata=substrate_db(p=p, DS="areal_units_input", redo=TRUE)
 
       sppoly = areal_units( p=p, redo=TRUE )  # this has already been done in aegis.polygons::01 polygons.R .. should nto have to redo
-      plot( sppoly[ "AUID" ] )
+
+    plt = areal_units( sppoly=sppoly, xydata=xydata, additional_features=additional_features, plotit=TRUE )
+    
+    (plt)
 
       # prepare data
       sppoly = areal_units( p=p  )  # this has already been done in aegis.polygons::01 polygons.R .. should nto have to redo
@@ -91,13 +102,6 @@
     carstm_plot_marginaleffects(  p=p, outputdir=oeffdir, fn_root_prefix=fn_root_prefix ) 
 
    
-  # bbox = c(-71.5, 41, -52.5,  50.5 )
-  additional_features = features_to_add( 
-      p=p, 
-      isobaths=c( 100, 200, 300, 400, 500  ), 
-      xlim=c(-80,-40), 
-      ylim=c(38, 60) 
-  )
 
   # maps of some of the results
   outputdir = file.path(p$modeldir, p$carstm_model_label, "maps" )
